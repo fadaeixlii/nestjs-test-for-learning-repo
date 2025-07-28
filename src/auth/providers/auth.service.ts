c69e8cadb4edb25fab5c81db57291a372cf772ef
fadaeixlii <mohammad4252@gmail.com>
2025-07-28 11:42:02 +0330

    Add swagger to project and some basic configurations

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   ) {}
 
   isAuth() {
-    return false;
+    return true;
   }
 
   login(authLoginDTO: AuthLoginDTO) {
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -11,6 +13,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setVersion('1.0')
+    .setTitle('Learning project')
+    .setDescription('This project for testing nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3130);
 }
 void bootstrap();
